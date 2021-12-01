4d721fc111a4ec5b1d6b7362bd0697af5cb00b1a
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-12-01 23:56:01 +0900

    タイトルつくった

---
+++ src/pages/chat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+
+const Chat: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Container>
+        <Typography>チャット</Typography>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default Chat;
